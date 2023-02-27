532d0a63d0510d6933b7ed890dfc3d0a7664f641
Alex Makarevych <a.makarevych@reply.de>
2023-02-27 12:25:01 +0100

    add component to upload patient image

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,29 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
+import { ChangeEvent, useState } from 'react';
+import FileUploadSingle from "./FileUploadSingle";
 
 function sayHello() {
   alert('This feature is under development!');
 }
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <h1>
           Connectome project
         </h1>
           <button onClick={sayHello}><h3>Upload patient image</h3></button>
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Learn React*/}
-        {/*</a>*/}
+        <FileUploadSingle/>
       </header>
     </div>
+
   );
 }
 
--- client-side/src/App.test.js
+++ client-side/src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ChangeEvent, useState } from 'react';
+
 
 test('renders learn react link', () => {
   render(<App />);
