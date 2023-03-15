fee2b9b21cd889be30bb5c1367f35e500c300b60
theYGE <alexandr.makarevych@gmail.com>
2023-03-15 18:16:49 +0100

    add docstrings

--- src/server-side/app.py
+++ src/server-side/app.py
@@ -1,3 +1,6 @@
+"""
+Backend of the project
+"""
 from flask import Flask
 import random
 from flask import jsonify
@@ -8,16 +11,21 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-# Landing page of the project
 @app.route("/")
 def hello_world():
+    """
+    Landing page of the project
+    @return: string
+    """
     return "Initial Page"
 
-# Main API method for handling API calls
 @app.route("/makePrediction", methods=["POST"])
 @cross_origin()
 def model_prediction():
-
+    """
+    Main API method for handling API calls
+    @return: int
+    """
     # Generates a demo value for the prototype
     probability = random.uniform(0,1)
     return jsonify(probability)
