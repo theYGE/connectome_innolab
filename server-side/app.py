21d7ee068ccdc6f3f23d2b934b38f87f0df99a87
theYGE <alexandr.makarevych@gmail.com>
2023-03-15 17:58:02 +0100

    ad comments

--- server-side/app.py
+++ server-side/app.py
@@ -3,23 +3,21 @@ import random
 from flask import jsonify
 from flask_cors import CORS, cross_origin
 
+# Initialization of the Flask App for the API calls
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+# Landing page of the project
 @app.route("/")
 def hello_world():
     return "Initial Page"
 
+# Main API method for handling API calls
 @app.route("/makePrediction", methods=["POST"])
 @cross_origin()
 def model_prediction():
 
-    #TODO: This is a temp solution
+    # Generates a demo value for the prototype
     probability = random.uniform(0,1)
     return jsonify(probability)
-    # return "<h1>Under development</h1>"
-
-    # TODO: Step 1: Get connectivity matrix
-    # TODO: Step 2: Run connectivity matrix through autoencoder
-    # TODO: Step 3: Get model output and return it
