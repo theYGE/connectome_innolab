41b05def8ca6168e28986e72557e773d3422d0af
SvenMaurice <SvenMauriceMorlock@yahoo.com>
2023-03-16 20:22:02 +0100

    added extensive information in main

--- src/connectome/main.py
+++ src/connectome/main.py
@@ -13,6 +13,20 @@ from train import vgae_graph_embedding
 def main(cfg: ConnectomeConfig) -> None:
     """Use this as entry point. Hydra decorator used to read config.yaml file.
 
+    For training a folder with connectivity matrices stored as pytorch tensors (.pt files) is needed.
+    The module 'utils.preprocess_matrices' provides functionalities for creating a folder with .pt files from a folder
+    with .csv files only. Checks on these folders can be performed using functionalities from 'utils.checks.py'.
+    Training results will be written in a dedicated assets-folder. During training a folder called 'checkpoints' will
+    be created storing models from each epoch as a pickle file (the common format used in 'torch.save()').
+    Further a folder called 'training_results' will be created which will store a csv file containing training and
+    validation error for each epoch. A hydra folder inside the assets folder stores yaml configurations as well as
+    tracked training results from hydra invoked by the decorator of this function.
+    The 'checkpoints' and 'training_results' folder may be cleaned before training setting 'clear-assets' to 'True'.
+    After training based on training and validation error the model from epoch with lowest validation error can
+    loaded using the function 'select_optimal_model' from utils module.
+
+
+
     Args:
         cfg (ConnectomeConfig): Hydra dictionary of configuration yaml file.
 
