3c1f2a034882170a7a21d2ee1606dd51613818f2
97Simei <lsmdlut@gmail.com>
2023-03-16 10:06:36 +0100

    update preprocess.py

--- examples/preprocessUKB/preprocess.py
+++ examples/preprocessUKB/preprocess.py
@@ -6,12 +6,10 @@ import sys
 from nilearn.maskers import NiftiMasker
 from nilearn.maskers import NiftiLabelsMasker
 from nilearn.connectome import ConnectivityMeasure
-from nilearn import plotting
 from nipype.interfaces import fsl
 from nipype.interfaces.fsl.maths import ApplyMask
 from nipype.testing import example_data
-from sklearn.covariance import GraphicalLassoCV
-from sklearn.covariance import GraphLassoCV as GraphicalLassoCV
+from sklearn.covariance import GraphicalLassoCV as GraphicalLassoCV
 def preprocess_ukb(
     path_atlas_file: str,
     path_normalize_refrence_file:str,
@@ -34,7 +32,6 @@ def preprocess_ukb(
     """
 
     participants = [folder for folder in os.listdir(participants_path) if os.path.isdir(os.path.join(participants_path, folder))]
-
     atlas = nib.load(path_atlas_file)
     masker = NiftiLabelsMasker(
         labels_img=atlas,
@@ -46,27 +43,42 @@ def preprocess_ukb(
     connectome_measure = ConnectivityMeasure(kind='correlation')
     for p in participants:
         # initialize some file path
-        path_input_file = os.path.join(participants_path, p,"filtered_func_data_clean_MNI.nii.gz")
+        path_input_file = os.path.join(participants_path, p,"filtered_func_data_clean.nii.gz")
         path_warp_file = os.path.join(participants_path, p,"example_func2standard_warp.nii.gz")
         path_output_file = os.path.join(participants_path, p,"filtered_func_data_clean_MNI.nii.gz")
         path_conn_result = os.path.join(path_out, p+".csv")
         # Nomalization step1: applywarp
         aw = fsl.ApplyWarp()
-        aw.inputs.in_file = example_data(path_input_file)
-        aw.inputs.ref_file = example_data(path_normalize_refrence_file)
-        aw.inputs.field_file = example_data(path_warp_file)
+        aw.inputs.in_file = path_input_file
+        aw.inputs.ref_file = path_normalize_refrence_file
+        aw.inputs.field_file = path_warp_file
         aw.inputs.interp = 'spline'
         aw.inputs.out_file = path_output_file
         applywarp_result = aw.run() 
         # Nomalization step2: fslmaths
-        fm = fsl.maths().ApplyMask()
+        fm = fsl.maths.ApplyMask()
         fm.inputs.in_file = example_data(path_output_file)
         fm.inputs.mask_file =example_data(path_mask_file)
         fm.inputs.out_file = path_output_file
         #compute connectivity matric
-        img = img = nib.load(path_output_file)
+        img = nib.load(path_output_file)
         time_series = masker.fit_transform(img)
         estimator = GraphicalLassoCV()
         estimator.fit(time_series)
         correlation_matrices = estimator.covariance_
         np.savetxt(path_conn_result, estimator.covariance_,delimiter=",")
+
+
+if __name__ == "__main__":
+    path_atlas_file = sys.argv[1]
+    path_normalize_refrence_file = sys.argv[2]
+    path_mask_file = sys.argv[3]
+    path_out = sys.argv[4]
+    participants_path = sys.argv[5]
+    preprocess_ukb(
+        path_atlas_file,
+        path_normalize_refrence_file,
+        path_mask_file,
+        path_out,
+        participants_path,
+    )
